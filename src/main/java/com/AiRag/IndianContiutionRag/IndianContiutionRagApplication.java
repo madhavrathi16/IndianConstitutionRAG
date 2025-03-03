69a11bdb521e55e52c2d8b4856fff0749529bde1
Madhav <madhavrathi16@gmail.com>
2025-03-04 02:22:13 +0530

    First commit this is basic skeleton of the project to start building on it.

---
+++ src/main/java/com/AiRag/IndianContiutionRag/IndianContiutionRagApplication.java
@@ -0,0 +1,14 @@
+package com.AiRag.IndianContiutionRag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndianContiutionRagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndianContiutionRagApplication.class, args);
+		System.out.println("here.....");
+	}
+
+}
